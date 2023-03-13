fac145a31c4b6131dd44f0eac185abc731caead1
D-Bop <official.og.owolabi@gmail.com>
2023-03-13 13:21:50 +0100

    first commit

---
+++ js excuse/Excuse.js
@@ -0,0 +1,64 @@
+const excuses = [
+    {
+        usecase: 'work',
+        work_excuse: 'I was breathing methane and hydrogen yesterday and my spirit could not find my shoes'
+    },
+
+    {
+        usecase: 'school',
+        school_excuse: 'The bridge connecting my house to the school got destroyed by a whale'
+    },
+
+    {
+        usecase: 'chores',
+        chores_excuse: 'My hands and legs disappeared for the whole day, they are just appearing now.'
+    },
+
+    {
+        usecase: 'church',
+        church_excuse: 'I dreamt about the service and i felt i was in church, i didnt know i was in my dream'
+    }
+]
+
+const balabu = excuses[0].work_excuse
+const blululu = excuses[0].usecase 
+
+const emilokan = excuses[1].school_excuse;
+const jagaban = excuses[1].usecase
+
+
+
+const infinix = excuses[2].chores_excuse;
+const tecno = excuses[2].usecase;
+
+const apple = excuses[3].church_excuse
+const samsung = excuses[3].usecase
+
+const excuse_form = document.querySelector('#excuse-form')
+const excuse_input = document.querySelector('#excuse')
+const btn = document.querySelector('.btn')
+const msg = document.querySelector('.msg');
+const excuse_list = document.querySelector('.excuses')
+
+
+excuse_form.addEventListener('submit', drop);
+console.log(balabu)
+function drop(ex) {
+    ex.preventDefault();
+
+    if(excuse_input.value === "") {
+        msg.innerHTML = "Please enter an excuse use";
+    }
+    else if (excuse_input.value.toLowerCase() === blululu) {
+        excuse_list.innerHTML = balabu
+    }
+    else if (excuse_input.value.toLowerCase() === jagaban) {
+        excuse_list.innerHTML = emilokan
+    }
+    else if (excuse_input.value.toLowerCase() === tecno) {
+        excuse_list.innerHTML = infinix
+    }
+    else if(excuse_input.value.toLowerCase() === samsung) {
+        excuse_list.innerHTML = apple 
+    }
+}
